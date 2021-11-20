5be4e060740c2117e8fff3540131708ff1b70620
larprad <loic.la.prairie@gmail.com>
2021-11-20 16:08:35 +0100

    Removing amplify configuration

--- src/component/Bibliotheque/index.tsx
+++ src/component/Bibliotheque/index.tsx
@@ -1,28 +1,8 @@
-import { API, graphqlOperation } from 'aws-amplify'
-import { useEffect, useState } from 'react'
-import { ListStorysQuery } from '../../graphql/API'
-import { listStorys } from '../../graphql/queries'
-
 const Biblotheque = (): JSX.Element => {
-  const [stories, setStories] = useState<ListStorysQuery | undefined>(undefined)
-
-  async function fetchStories() {
-    try {
-      const apiData = (await API.graphql(graphqlOperation(listStorys))) as { data: ListStorysQuery }
-      setStories(apiData.data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchStories()
-  }, [])
 
   return (
     <div>
       <h2>Biblotheque page</h2>
-      {stories && stories.listStorys?.items.map((story) => <li key={story.id}>{story.title}</li>)}
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import Amplify from 'aws-amplify'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
-import awsconfig from './aws-exports'
 
 import App from './component/App/App'
 import './assets/style/index.css'
-
-Amplify.configure(awsconfig)
 
 ReactDOM.render(
   <React.StrictMode>
