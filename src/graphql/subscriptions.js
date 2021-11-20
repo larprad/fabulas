e628a174f17fba9471bc8ff8e528be61eb1c372d
larprad <loic.la.prairie@gmail.com>
2021-11-20 20:09:27 +0100

    Creating new amplify graphQL api again

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateStory = /* GraphQL */ `
+  subscription OnCreateStory {
+    onCreateStory {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateStory = /* GraphQL */ `
+  subscription OnUpdateStory {
+    onUpdateStory {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteStory = /* GraphQL */ `
+  subscription OnDeleteStory {
+    onDeleteStory {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
